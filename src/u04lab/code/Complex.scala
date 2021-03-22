0161a4c72e39bcf7fdd823886e2f68c4b011ecab
ndido98 <ndido98@gmail.com>
2021-03-22 11:31:55 +0100

    Add implementation of complex numbers

--- src/u04lab/code/Complex.scala
+++ src/u04lab/code/Complex.scala
@@ -5,20 +5,18 @@ trait Complex {
 
     def im: Double
 
-    def +(c: Complex): Complex // should implement the sum of two complex numbers..
+    def +(c: Complex): Complex // should implement the sum of two complex numbers
     def *(c: Complex): Complex // should implement the product of two complex numbers
 }
 
 object Complex {
-    def apply(re: Double, im: Double): Complex = ??? // Fill here
-}
+    def apply(re: Double, im: Double): Complex = ComplexImpl(re, im)
 
-object TryComplex extends App {
-    val a = Array(Complex(10, 20), Complex(1, 1), Complex(7, 0))
-    val c = a(0) + a(1) + a(2)
-    println(c, c.re, c.im) // (ComplexImpl(18.0,21.0),18.0,21.0)
-    val c2 = a(0) * a(1)
-    println(c2, c2.re, c2.im) // (ComplexImpl(-10.0,30.0),-10.0,30.0)
+    private case class ComplexImpl(re: Double, im: Double) extends Complex {
+        override def +(c: Complex): Complex = Complex(re + c.re, im + c.im)
+
+        override def *(c: Complex): Complex = Complex(re * c.re - im * c.im, im * c.re + re * c.im)
+    }
 }
 
 /** Hints:
