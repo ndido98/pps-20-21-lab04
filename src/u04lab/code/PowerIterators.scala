b30bc3a55496a86b85c46729714ad9a802694fcd
ndido98 <ndido98@gmail.com>
2021-03-22 12:47:51 +0100

    Implement power iterators

--- src/u04lab/code/PowerIterators.scala
+++ src/u04lab/code/PowerIterators.scala
@@ -2,6 +2,9 @@ package u04lab.code
 
 import Optionals._
 import Lists._
+import Streams._
+
+import scala.util.Random
 
 trait PowerIterator[A] {
     def next(): Option[A]
@@ -15,7 +18,7 @@ trait PowerIteratorsFactory {
 
     def incremental(start: Int, successive: Int => Int): PowerIterator[Int]
 
-    def fromList[A](list: List[A])
+    def fromList[A](list: List[A]): PowerIterator[A]
 
     def randomBooleans(size: Int): PowerIterator[Boolean]
 }
@@ -25,10 +28,34 @@ object PowerIteratorsFactory {
 
     private class PowerIteratorsFactoryImpl extends PowerIteratorsFactory {
 
-        override def incremental(start: Int, successive: Int => Int): PowerIterator[Int] = ???
+        override def incremental(start: Int, successive: Int => Int): PowerIterator[Int] =
+            fromStream(Stream.iterate(start)(successive))
 
-        override def fromList[A](list: List[A]): Unit = ???
+        override def fromList[A](list: List[A]): PowerIterator[A] = ???
 
-        override def randomBooleans(size: Int): PowerIterator[Boolean] = ???
+        override def randomBooleans(size: Int): PowerIterator[Boolean] =
+            fromStream(Stream.take(Stream.generate(Random.nextBoolean()))(size))
+
+        private def fromStream[A](stream: Stream[A]): PowerIterator[A] = new PowerIteratorImpl(stream)
+    }
+
+    private class PowerIteratorImpl[A](private var stream: Stream[A]) extends PowerIterator[A] {
+        var past: List[A] = List.Nil()
+
+        override def next(): Option[A] = {
+            val first = Stream.first(stream)
+            first match {
+                case Option.Some(x) => {
+                    past = List.append(past, List.Cons(x, List.Nil()))
+                    stream = Stream.skip(stream)(1)
+                    first
+                }
+                case Option.None() => first
+            }
+        }
+
+        override def allSoFar(): List[A] = past
+
+        override def reversed(): PowerIterator[A] = new PowerIteratorImpl(Stream.fromList(List.reverse(past)))
     }
 }
--- test/u04lab/code/PowerIteratorsTests.scala
+++ test/u04lab/code/PowerIteratorsTests.scala
@@ -17,15 +17,15 @@ class PowerIteratorsTests {
         assertEquals(Option.of(7), pi.next)
         assertEquals(Option.of(9), pi.next)
         assertEquals(Option.of(11), pi.next)
-        assertEquals(List.Cons(5, List.Cons(7, List.Cons(9, List.Cons(11, List.Nil())))), pi.allSoFar()) // elementi già prodotti
+        assertEquals(List.Cons(5, List.Cons(7, List.Cons(9, List.Cons(11, List.Nil())))), pi.allSoFar) // elementi già prodotti
         for (_ <- 0 until 10) {
             pi.next() // procedo in avanti per un po'...
         }
-        assertEquals(Option.of(33), pi.next()) // sono arrivato a 33
+        assertEquals(Option.of(33), pi.next) // sono arrivato a 33
     }
 
-   @Test
-   def testRandom(): Unit = { // semi-automatico, si controlleranno anche le stampe a video
+    @Test
+    def testRandom(): Unit = { // semi-automatico, si controlleranno anche le stampe a video
         val pi = factory.randomBooleans(4) // pi produce 4 booleani random
         val b1 = pi.next
         val b2 = pi.next
@@ -33,8 +33,12 @@ class PowerIteratorsTests {
         val b4 = pi.next
         println(b1 + " " + b2 + " " + b3 + " " + b4) // stampo a video... giusto per vedere se sono proprio random...
 
-        assertFalse(!Option.isEmpty(pi.next)) // ne ho già prodotti 4, quindi il prossimo è un opzionale vuoto
+        assertTrue(Option.isEmpty(pi.next)) // ne ho già prodotti 4, quindi il prossimo è un opzionale vuoto
 
-        assertEquals(pi.allSoFar, List.Cons(b1, List.Cons(b2, List.Cons(b3, List.Cons(b4, Nil()))))) // ho prodotto proprio b1,b2,b3,b4
+        val b1val = Option.get(b1)
+        val b2val = Option.get(b2)
+        val b3val = Option.get(b3)
+        val b4val = Option.get(b4)
+        assertEquals(List.Cons(b1val, List.Cons(b2val, List.Cons(b3val, List.Cons(b4val, Nil())))), pi.allSoFar) // ho prodotto proprio b1,b2,b3,b4
     }
 }
